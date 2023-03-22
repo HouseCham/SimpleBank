9b8c39fdf93c0f2b2da09821b4a50ed44f61c44d
HouseCham <ramsesramirezvallejo@gmail.com>
2023-03-22 01:13:41 -0600

    readme file updated

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
+![Logo](https://blog.jetbrains.com/wp-content/uploads/2021/02/Go_8001611039611515.gif)
+
+
 # Welcome to Simple Bank repository
 
-#### This is a final project part of the [Golang Backend Master Class](https://www.udemy.com/course/backend-master-class-golang-postgresql-kubernetes/) from Udemy
+This is a final project part of the [Golang Backend Master Class](https://www.udemy.com/course/backend-master-class-golang-postgresql-kubernetes/) from Udemy
 
-##### This project uses:
+![GoVersion](https://img.shields.io/static/v1?label=Go&message=v1.2&color=blue)
+![PostgreSQL](https://img.shields.io/static/v1?label=PostgreSQL&message=v12-alpine&color=blue)
 
-* Golang 1.2
-* [PostgreSQL](https://www.postgresql.org/) as database motor
-* [Sqlc](https://github.com/kyleconroy/sqlc/blob/main/docs/index.rst)
 
+The main porpuse of this project is to learn everything about backend web development: Golang, Postgres, Redis, Gin, gRPC, Docker, Kubernetes, AWS, CI/CD.
--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -12,14 +12,12 @@ type Store struct {
 	*Queries
 	db *sql.DB
 }
-
 // TransferTxParams contains the input parameters of the transfer transaction
 type TransferTxParams struct {
 	FromAccountId int64   `json:"from_account_id"`
 	ToAccountId   int64   `json:"to_account_id"`
 	Amount        float64 `json:"amount"`
 }
-
 // TransferTxResult is the result of the transfer transaction
 type TransferTxResult struct {
 	Transfer    Transfer `json:"transfer"`
@@ -28,7 +26,6 @@ type TransferTxResult struct {
 	FromEntry   Entry    `json:"from_entry"`
 	ToEntry     Entry    `json:"to_entry"`
 }
-
 // NewStore creates a new Store
 func NewStore(db *sql.DB) *Store {
 	return &Store{
@@ -36,7 +33,6 @@ func NewStore(db *sql.DB) *Store {
 		Queries: New(db),
 	}
 }
-
 // execTx executes a function within a database transaction
 func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
@@ -53,7 +49,6 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 
 	return tx.Commit()
 }
-
 // TransferTx performs a money transfer from one account to the other.
 // It creates a transfer record, add account entries, and update accounts' balance within a single database transaction
 func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
@@ -91,9 +86,13 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 		}
 
 		//! TODO: update accounts' balance
-		
 
 		return nil
 	})
 
+	if err != nil {
+		return result, err
+	}
+
+	return result, nil
 }
